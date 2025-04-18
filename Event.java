e046ec7ab2f07667c5a141c233742f32b3371f3e
MoraxTale <suemorax@foxmail.com>
2025-04-18 19:34:52 +0800

    Add files via upload
    4.18.4new

---
+++ Event.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import java.util.List;
+
+public class Event {
+    private String name;
+    private String description;
+    private List<EventOption> options;
+
+    public Event(String name, String description, List<EventOption> options) {
+        this.name = name;
+        this.description = description;
+        this.options = options;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<EventOption> getOptions() {
+        return options;
+    }
+}
