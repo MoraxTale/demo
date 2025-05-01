1001d79b78db8e3e594d89d49dda4fa84a7abe88
Chenyu-cpu <2764557916@qq.com>
2025-05-01 16:41:27 +0800

    Update HelloApplication.java

--- HelloApplication.java
+++ HelloApplication.java
@@ -6,17 +6,29 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.UUID;
+
 
 public class HelloApplication extends Application {
+    private MusicPlayer musicPlayer;
     @Override
     public void start(Stage stage) throws IOException {
+        // 初始化音乐播放器
+        musicPlayer = new MusicPlayer("E:\\JAVA FX\\demo\\Music\\main.mp3");
+        musicPlayer.setVolume(0.5); // 设置音量
+        musicPlayer.play(); // 播放音乐
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 960, 580);
         stage.setTitle("我独自修仙");
         stage.setScene(scene);
         stage.show();//
     }
-
+    @Override
+    public void stop() {
+        if (musicPlayer != null) {
+            musicPlayer.stop();
+        }
+    }
     public static void main(String[] args) {
         launch();
     }
